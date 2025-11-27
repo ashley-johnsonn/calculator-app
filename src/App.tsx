202968d3675c0578e798d00fa6eb429b0ad0609a
Ashley Johnson <ashley.duysen@gmail.com>
2025-11-27 11:25:56 +1000

    moved buttons into their own component

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import "./App.css";
-import { Button } from "./CalculatorButton";
+import { CalculatorButtons } from "./CalculatorButtons";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -8,65 +8,7 @@ function App() {
   return (
     <>
       <h1>Calculator 🌸</h1>
-      <div className="calculator-buttons">
-        <Button onClick={() => {}} color="#CEA6B1">
-          7
-        </Button>
-        <Button onClick={() => {}} color="#CEA6B1">
-          4
-        </Button>
-        <Button onClick={() => {}} color="#CEA6B1">
-          1
-        </Button>
-        <Button onClick={() => {}} color="#CEA6B1">
-          C
-        </Button>
-        <Button onClick={() => {}} color="#CEA6B1">
-          8
-        </Button>
-        <Button onClick={() => {}} color="#CEA6B1">
-          5
-        </Button>
-        <Button onClick={() => {}} color="#CEA6B1">
-          2
-        </Button>
-        <Button onClick={() => {}} color="#CEA6B1">
-          0
-        </Button>
-        <Button onClick={() => {}} color="#CEA6B1">
-          9
-        </Button>
-        <Button onClick={() => {}} color="#CEA6B1">
-          6
-        </Button>
-        <Button onClick={() => {}} color="#CEA6B1">
-          3
-        </Button>
-        <Button onClick={() => {}} color="#CEA6B1">
-          .
-        </Button>
-        <Button onClick={() => {}} color="#CEA6B1">
-          +/-
-        </Button>
-        <Button onClick={() => {}} color="#CEA6B1">
-          x
-        </Button>
-        <Button onClick={() => {}} color="#CEA6B1">
-          -
-        </Button>
-        <Button onClick={() => {}} color="#CEA6B1">
-          +
-        </Button>
-        <Button onClick={() => {}} color="#CEA6B1">
-          ←
-        </Button>
-        <Button onClick={() => {}} color="#CEA6B1">
-          ÷
-        </Button>
-        <Button onClick={() => {}} color="#C697A4">
-          =
-        </Button>
-      </div>
+      <CalculatorButtons />
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
---
+++ src/CalculatorButtons.tsx
@@ -0,0 +1,65 @@
+import { Button } from "./CalculatorButton";
+
+export function CalculatorButtons() {
+  return (
+    <div className="calculator-buttons">
+      <Button onClick={() => {}} color="#CEA6B1">
+        7
+      </Button>
+      <Button onClick={() => {}} color="#CEA6B1">
+        4
+      </Button>
+      <Button onClick={() => {}} color="#CEA6B1">
+        1
+      </Button>
+      <Button onClick={() => {}} color="#CEA6B1">
+        C
+      </Button>
+      <Button onClick={() => {}} color="#CEA6B1">
+        8
+      </Button>
+      <Button onClick={() => {}} color="#CEA6B1">
+        5
+      </Button>
+      <Button onClick={() => {}} color="#CEA6B1">
+        2
+      </Button>
+      <Button onClick={() => {}} color="#CEA6B1">
+        0
+      </Button>
+      <Button onClick={() => {}} color="#CEA6B1">
+        9
+      </Button>
+      <Button onClick={() => {}} color="#CEA6B1">
+        6
+      </Button>
+      <Button onClick={() => {}} color="#CEA6B1">
+        3
+      </Button>
+      <Button onClick={() => {}} color="#CEA6B1">
+        .
+      </Button>
+      <Button onClick={() => {}} color="#CEA6B1">
+        +/-
+      </Button>
+      <Button onClick={() => {}} color="#CEA6B1">
+        x
+      </Button>
+      <Button onClick={() => {}} color="#CEA6B1">
+        -
+      </Button>
+      <Button onClick={() => {}} color="#CEA6B1">
+        +
+      </Button>
+      <Button onClick={() => {}} color="#CEA6B1">
+        ←
+      </Button>
+      <Button onClick={() => {}} color="#CEA6B1">
+        ÷
+      </Button>
+      <Button onClick={() => {}} color="#C697A4">
+        =
+      </Button>
+    </div>
+  );
+}
