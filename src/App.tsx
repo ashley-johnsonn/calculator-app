a2b91f07829c2371269bb053c2a1bca98a356d51
Ashley Johnson <ashley.duysen@gmail.com>
2025-11-27 11:23:37 +1000

    fixed button spacing

--- src/App.tsx
+++ src/App.tsx
@@ -12,29 +12,59 @@ function App() {
         <Button onClick={() => {}} color="#CEA6B1">
           7
         </Button>
-        <Button onClick={() => {}} color="#CB9EAA">
-          8
-        </Button>
-        <Button onClick={() => {}} color="#C697A4">
-          9
-        </Button>
         <Button onClick={() => {}} color="#CEA6B1">
           4
-        </Button>
-        <Button onClick={() => {}} color="#CB9EAA">
-          5
-        </Button>
-        <Button onClick={() => {}} color="#C697A4">
-          6
         </Button>
         <Button onClick={() => {}} color="#CEA6B1">
           1
         </Button>
-        <Button onClick={() => {}} color="#CB9EAA">
+        <Button onClick={() => {}} color="#CEA6B1">
+          C
+        </Button>
+        <Button onClick={() => {}} color="#CEA6B1">
+          8
+        </Button>
+        <Button onClick={() => {}} color="#CEA6B1">
+          5
+        </Button>
+        <Button onClick={() => {}} color="#CEA6B1">
           2
         </Button>
-        <Button onClick={() => {}} color="#C697A4">
+        <Button onClick={() => {}} color="#CEA6B1">
+          0
+        </Button>
+        <Button onClick={() => {}} color="#CEA6B1">
+          9
+        </Button>
+        <Button onClick={() => {}} color="#CEA6B1">
+          6
+        </Button>
+        <Button onClick={() => {}} color="#CEA6B1">
           3
+        </Button>
+        <Button onClick={() => {}} color="#CEA6B1">
+          .
+        </Button>
+        <Button onClick={() => {}} color="#CEA6B1">
+          +/-
+        </Button>
+        <Button onClick={() => {}} color="#CEA6B1">
+          x
+        </Button>
+        <Button onClick={() => {}} color="#CEA6B1">
+          -
+        </Button>
+        <Button onClick={() => {}} color="#CEA6B1">
+          +
+        </Button>
+        <Button onClick={() => {}} color="#CEA6B1">
+          ←
+        </Button>
+        <Button onClick={() => {}} color="#CEA6B1">
+          ÷
+        </Button>
+        <Button onClick={() => {}} color="#C697A4">
+          =
         </Button>
       </div>
       <div className="card">
