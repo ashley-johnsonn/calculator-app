9845f9a6bef87ba4a48ed6a19db0c49001062942
Ashley Johnson <ashley.duysen@gmail.com>
2025-11-27 16:12:35 +1000

    stylinggggg

--- src/NumberDisplay.tsx
+++ src/NumberDisplay.tsx
@@ -7,13 +7,14 @@ export const NumberDisplay: React.FC<DisplayProps> = ({ value }) => {
     <div
       style={{
         padding: "20px",
-        fontSize: "32px",
+        fontSize: "40px",
         backgroundColor: "#CEA6B1",
-        border: "2px solid #333",
         borderRadius: "5px",
         textAlign: "right",
         minHeight: "60px",
         fontFamily: "monospace",
+        width: "510px",
+        marginBottom: "10px",
       }}
     >
       {value || "0"}
